e075cb3f2f876669deafbc426e7b703a0f0ccf5e
RedAFD <radixholms@gmail.com>
2021-03-02 20:02:47 +0800

    add application

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/RedAFD/lim/pkg/logger"
+	"github.com/RedAFD/lim/pkg/server"
+)
+
+var ip = flag.String("ip", "127.0.0.1", "input the IP you want to listen to")
+var port = flag.String("port", "7714", "input the port you want to listion to")
+
+func main() {
+	flag.Parse()
+	addr := *ip + ":" + *port
+	logger.Info("you are listening %s", addr)
+	s := server.NewServer(addr)
+	if err := s.ListenAndServe(); err != nil {
+		logger.Fatal("failed to serve: %v", err)
+	}
+}
